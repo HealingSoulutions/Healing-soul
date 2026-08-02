5bd46469a2fbbef456d09a6dc2fac010fe2c83cb
HealingSoulutions <info@healingsoulutions.care>
2026-08-01 21:23:52 -0400

    Update _document.js

--- healing-soul/pages/_document.js
+++ healing-soul/pages/_document.js
@@ -46,7 +46,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Quicksand:wght@400;500;600;700&family=Comic+Neue:ital,wght@0,400;0,700;1,400;1,700&family=Varela+Round&family=Outfit:wght@300;400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500&family=Varela+Round&family=Outfit:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
